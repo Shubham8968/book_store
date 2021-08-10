5ea7be96378d3317ae50adfcb88096daee81db77
Shubham8968 <Shivamno1420@gmail.com>
2021-08-10 15:13:41 +0530

    this is my Bookstore css and js file

--- src/firstcomponent.js
+++ src/firstcomponent.js
@@ -7,6 +7,7 @@ function FirstComponent()
         <React.Fragment>
             <div>
                 <h2>This is my first component </h2>
+                <img src="pug_dog_blanket_110448_3840x2400.jpg" widht="700" height="500"></img>
             </div>
         </React.Fragment>
     )
---
+++ src/second component.js
@@ -0,0 +1,18 @@
+import React from "react";
+import{render} from "react-dom";
+
+function secondcomponent()
+{
+    return(
+        <React.Fragment>
+            <div>
+                <h2>Shubham </h2>
+                <h2> 22 </h2>
+                <img src="images.jpg" widht="600" height="500"></img>
+            </div>
+           
+        </React.Fragment>
+    )
+}
+
+export default secondcomponent
